3e0d93623c2e357ad190f331eaa0366e3e91b0ab
mdemo <mds@xue.bi>
2015-07-14 10:53:19 +0800

    init project

---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var Router = require('koa-router');
+var compose = require('koa-compose');
+var logger = require('bd-logger');
+var path = require('path');
+var fs = require('fs');
+
+var routers = [];
+var loggers = {};
+
+module.exports = function (apps) {
+  var keys = Object.keys(apps);
+  keys.map(function (key) {
+    var dirname = apps[key];
+    var prefix = (key == 'index') ? '' : key;
+    var router = new Router({
+      prefix: '/' + prefix
+    });
+    var routesDirname = path.join(dirname, 'routes');
+    if (fs.existsSync(routesDirname)) {
+      if (!loggers[key]) {
+        loggers[key] = logger(key);
+      }      
+      var routes = fs.readdirSync(routesDirname);
+      routes.map(function (file) {
+        var route = require(path.join(routesDirname, file));
+        if(typeof route == 'function'){
+          route(router, loggers[key])
+        }
+      })
+    }
+    routers.push(router.routes());
+  });
+  return compose(routers);
+};
