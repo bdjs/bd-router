9fa62917ab3e5cb131b329cb6ec4857d8370410d
mdemo <mds@xue.bi>
2015-07-21 10:02:27 +0800

    Ignore .**file

--- index.js
+++ index.js
@@ -9,28 +9,33 @@ var fs = require('fs');
 var routers = [];
 var loggers = {};
 
-module.exports = function (apps) {
-  var keys = Object.keys(apps);
-  keys.map(function (key) {
-    var dirname = apps[key];
-    var prefix = (key == 'index') ? '' : key;
-    var router = new Router({
-      prefix: '/' + prefix
-    });
-    var routesDirname = path.join(dirname, 'routes');
-    if (fs.existsSync(routesDirname)) {
-      if (!loggers[key]) {
-        loggers[key] = logger({app: key});
-      }      
-      var routes = fs.readdirSync(routesDirname);
-      routes.map(function (file) {
-        var route = require(path.join(routesDirname, file));
-        if(typeof route == 'function'){
-          route(router, loggers[key])
+module.exports = function(apps) {
+    var keys = Object.keys(apps);
+    keys.map(function(key) {
+        var dirname = apps[key];
+        var prefix = (key == 'index') ? '' : key;
+        var router = new Router({
+            prefix: '/' + prefix
+        });
+        var routesDirname = path.join(dirname, 'routes');
+        if (fs.existsSync(routesDirname)) {
+            if (!loggers[key]) {
+                loggers[key] = logger({
+                    app: key
+                });
+            }
+            var routes = fs.readdirSync(routesDirname);
+            routes.map(function(file) {
+                if (file[0] === '.') {
+                    return;
+                }
+                var route = require(path.join(routesDirname, file));
+                if (typeof route == 'function') {
+                    route(router, loggers[key]);
+                }
+            });
         }
-      })
-    }
-    routers.push(router.routes());
-  });
-  return compose(routers);
+        routers.push(router.routes());
+    });
+    return compose(routers);
 };
